d79c25e7ea2e5c82eca3585eb53c96eccf0e799f
Moe-Myat-Thwe <moemyatthwe382@gmail.com>
2025-07-25 13:42:50 +0630

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -6,41 +6,44 @@ def calculate(list):
     ls = np.array(list)
     ls = ls.reshape(3,3)
 
-    mean_rows = np.mean(ls, axis=1)
-    mean_columns = np.mean(ls, axis=0)
 
-    var_rows = np.var(ls, axis=1)
-    var_columns = np.var(ls, axis=0)
+    mean_row = np.mean(ls, axis=1)
+    mean_column = np.mean(ls, axis=0)
+    mean_flattened = np.mean(ls)
 
-    std_rows = np.std(ls, axis=1)
-    std_columns = np.std(ls, axis=0)
+    var_row = np.var(ls, axis=1)
+    var_column = np.var(ls, axis=0)
+    var_flattened = np.var(ls)
 
-    max_rows = np.max(ls, axis=1)
-    max_columns = np.max(ls, axis=0)
+    std_row = np.std(ls, axis=1)
+    std_column = np.std(ls, axis=0)
+    std_flattened = np.std(ls)
 
-    min_rows = np.min(ls, axis=1)
-    min_columns = np.min(ls, axis=0)
+    max_row = np.max(ls, axis=1)
+    max_column = np.max(ls, axis=0)
+    max_flattened = np.max(ls)
 
-    sum_rows = np.sum(ls, axis=1)
-    sum_columns = np.sum(ls, axis=0)
+    min_row = np.min(ls, axis=1)
+    min_column = np.min(ls, axis=0)
+    min_flattened = np.min(ls)
 
-    results = {'mean': [mean_columns, mean_rows, np.mean(ls)],
-            'variance': [var_columns, var_rows, np.var(ls)],
-            'standard deviation': [std_columns, std_rows, np.std(ls)],
-            'max': [max_columns, max_rows, np.max(ls)],
-            'min': [min_columns, min_rows, np.min(ls)],
-            'sum': [sum_columns, sum_rows, np.sum(ls)]}
+    sum_row = np.sum(ls, axis=1)
+    sum_column = np.sum(ls, axis=0)
+    sum_flattened = np.sum(ls)
 
-    fixed_results = {}
+    results = {'mean': [mean_column, mean_row, mean_flattened],
+               'variance': [var_column, var_row, var_flattened],
+               'standard deviation': [std_column, std_row, std_flattened],
+               'max': [max_column, max_row, max_flattened],
+               'min': [min_column, min_row, min_flattened],
+               'sum': [sum_column, sum_row, sum_flattened] }
+
+    fix_results = {}
     for key, value_list in results.items():
-        fixed_list = []
+        fix_values = []
         for item in value_list:
-            if isinstance(item, np.ndarray):
-                fixed_list.append(item.tolist())
-            elif isinstance(item, (np.float64, np.int64)):
-                fixed_list.append(item.item())
-            else:
-                fixed_list.append(item)
-        fixed_results[key] = fixed_list
-    return fixed_results
+            fix_values.append(item.tolist())
+            fix_results[key] = fix_values
+    return fix_results
+
 
